be7eadbabb4134cbeb3480b2c4ff4c83aad4e5da
KennV <kenn.villegas@me.com>
2024-10-01 11:49:57 -0400

    Added Project
    Basic default with tests and No Data

---
+++ Aardwolf/Aardwolf/AardwolfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AardwolfApp.swift
+//  Aardwolf
+//
+//  Created by Kenn Villegas on 10/1/24.
+//
+
+import SwiftUI
+
+@main
+struct AardwolfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Aardwolf/Aardwolf/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Aardwolf
+//
+//  Created by Kenn Villegas on 10/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
